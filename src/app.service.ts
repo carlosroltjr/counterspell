c51337630f54caf172ac9f6ed8630a2c7cb6bba3
Carlos <carlosroltjr@gmail.com>
2024-08-26 18:47:45 -0300

    filtering matches by lobby type and rank

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,24 @@
 import { HttpService } from '@nestjs/axios'
 import { Injectable, Logger } from '@nestjs/common'
-import { Cron } from '@nestjs/schedule'
+import { Cron, CronExpression } from '@nestjs/schedule'
 import { PrismaService } from './database/prisma.service'
 
 @Injectable()
 export class AppService {
-  constructor (private httpService: HttpService,
+  constructor (
+    private httpService: HttpService,
     private prisma: PrismaService
   ) {}
 
   private readonly logger = new Logger(AppService.name)
-  private readonly maxRequestsPerDay: number = 5
+  private readonly maxRequestsPerDay: number = 2000
   private startingMatchIdValue: number
 
   getHello (): string {
     return 'Hello World!'
   }
 
-  @Cron('*/2 * * * *')
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async saveMatches () {
     this.logger.log('Starting data extract stage...')
     let remainingRequests = this.maxRequestsPerDay
@@ -25,24 +26,39 @@ export class AppService {
     while (remainingRequests > 0) {
       const openDotaMatches = await this.httpService.axiosRef.get(
         process.env.OPEN_DOTA_BASE_URL + '/publicMatches',
-        { params: { less_than_match_id: lowestMatchId } }
+        { params: { less_than_match_id: lowestMatchId, min_rank: 20 } }
       )
 
-      openDotaMatches.data.forEach((match: { match_id: number }) => {
+      const filteredMatches = openDotaMatches.data.filter(
+        (match: { lobby_type: number }) =>
+          match.lobby_type === 5 || match.lobby_type === 6
+      )
+
+      filteredMatches.forEach((match: { match_id: number }) => {
         if (lowestMatchId > match.match_id) {
           lowestMatchId = match.match_id
         }
       })
 
-      await this.prisma.matches.create({
-        data: { data: openDotaMatches.data }
-      }).then(matches => {
-        this.logger.log(`Saved matches[] with id ${matches.id} to database...`)
-      })
+      const matchesDay: Date = new Date(
+        openDotaMatches.data[0].start_time * 1000
+      )
+      await this.prisma.matches
+        .create({
+          data: {
+            matchesDay,
+            data: filteredMatches
+          }
+        })
+        .then((matches) => {
+          this.logger.log(
+            `Saved matches[] with id ${matches.id} to database...`
+          )
+        })
 
       remainingRequests--
       this.logger.debug(`Remaining requests: ${remainingRequests}.`)
-      await new Promise<void>(resolve => setTimeout(() => resolve(), 1000))
+      await new Promise<void>((resolve) => setTimeout(() => resolve(), 1000))
     }
     this.startingMatchIdValue = lowestMatchId
   }
